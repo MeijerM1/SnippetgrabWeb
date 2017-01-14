3263f5544f732a525fde2725d732d4e856bd9f7b
Max <Max@MAX-PC>
2017-01-14 11:23:17 +0100

    Changed startup

--- Snippetgrab/Snippetgrab/Global.asax.cs
+++ Snippetgrab/Snippetgrab/Global.asax.cs
@@ -9,18 +9,6 @@ namespace Snippetgrab
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default",                                              // Route name 
-                "{User}/{Login}",                           // URL with parameters 
-                new { controller = "User", action = "LogIn"}  // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
